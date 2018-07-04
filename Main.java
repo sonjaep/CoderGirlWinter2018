fb5a6c53237a917d6cc53cc56cc62ef658ddd896
sonjaep <40833325+sonjaep@users.noreply.github.com>
2018-07-04 11:20:42 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, CoderGirls2018! This is my first ever code!");
+    }
+}
